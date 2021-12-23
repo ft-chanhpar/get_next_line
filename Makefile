4de2d775e8e11ec78a5545e7399e8d700440d9ab
chanhpar@home <chanhpar@DESKTOP-4USP1BM.localdomain>
2021-12-23 22:23:42 +0900

    Practicing open, read, write

---
+++ Makefile
@@ -0,0 +1,47 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: chanhpar <chanhpar@student.42seoul.kr>     +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2021/10/27 02:20:40 by chanhpar          #+#    #+#              #
+#    Updated: 2021/12/23 22:20:50 by chanhpar         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+SRC	= get_next_line.c get_next_line_utils.c
+
+BSRC	= 
+BSRCDIR	=
+
+OBJ		= $(SRC:.c=.o)
+BOBJ	= $(BSRC:.c=.o)
+
+INC		=
+
+NAME	= a.out
+
+CC		= gcc
+CFLAGS	= -Wall -Wextra -Werror
+
+all: $(NAME)
+
+$(NAME): $(OBJ)
+	$(CC) $(CFLAGS) main.c $^
+
+bonus:
+	@make --no-print-directory OBJ="$(OBJ) $(BOBJ)" all
+
+%.o: %.c
+	$(CC) $(CFLAGS) -c $< -o $(<:.c=.o) -D BUFFER_SIZE=42
+
+clean:
+	rm -f $(OBJ) $(BOBJ)
+
+fclean: clean
+	rm -f $(NAME)
+
+re: fclean all
+
+.PHONY: all bonus clean fclean re 
--- main.c
+++ main.c
@@ -1,33 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: chanhpar <chanhpar@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/21 03:25:35 by chanhpar          #+#    #+#             */
-/*   Updated: 2021/12/23 22:21:16 by chanhpar         ###   ########.fr       */
+/*   Updated: 2021/12/23 22:21:46 by chanhpar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*get_next_line(int fd)
+int	main(void)
 {
-	static size_t	i;
-	static size_t	buff;
-	static char		*c;
-	ssize_t			rd;
+	static char	*str;
+	int	i = 0;
+	int	fd;
 
-	buff = BUFFER_SIZE;
-	if (fd < 0)
-		return (NULL);
-	c = (char *)malloc(sizeof(char) * buff);
-	rd = read(fd, &c[i], 1);
-	write(1, &c[i], 1);
-	write(1, "\n", 1);
-	i++;
-	if (rd < 0)
-		return (NULL);
-	return (c);
+	fd = open("sample", O_RDONLY);
+	while (i < 5)
+	{
+		str = get_next_line(fd);
+		i++;
+	}
+	write(1, str, 5);
+	free(str);	
+	return (0);
 }
