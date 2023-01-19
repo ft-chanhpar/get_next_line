73f371a5f8e74d6ad3a673be656fc07f0eb80ea7
chanhpar <chanhpar@student.42seoul.kr>
2023-01-19 22:04:26 +0900

    WIP: gnl

--- get_next_line.c
+++ get_next_line.c
@@ -6,16 +6,38 @@
 /*   By: chanhpar <chanhpar@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 14:28:27 by chanhpar          #+#    #+#             */
-/*   Updated: 2023/01/19 14:29:52 by chanhpar         ###   ########.fr       */
+/*   Updated: 2023/01/19 21:39:26 by chanhpar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdlib.h>
+#include <unistd.h>
+
+char	*gnl(t_node *node, int fd)
+{
+	ssize_t	read_len;
+	char	buffer[BUFFER_SIZE];
+
+	if (node->fd == fd)
+	{
+	}
+	if (node->next)
+		return (gnl(node->next, fd));
+	node->next = malloc(sizeof(t_node));
+	if (node->next == NULL)
+		return (NULL);
+	node->next->fd = fd;
+	node->next->state = EMPTY;
+	return (gnl(node->next, fd));
+}
 
 char	*get_next_line(int fd)
 {
-	if (fd < 0)
+	static t_node	head;
+	char			*ret;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	return (NULL);
+	return (gnl(&head, fd));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: chanhpar <chanhpar@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 14:27:50 by chanhpar          #+#    #+#             */
-/*   Updated: 2023/01/19 16:05:30 by chanhpar         ###   ########.fr       */
+/*   Updated: 2023/01/19 20:46:14 by chanhpar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 1024
 # endif
+
+# include <stddef.h>
 
 typedef enum e_state
 {
@@ -28,6 +30,9 @@ typedef struct s_node
 	struct s_node	*next;
 	int				fd;
 	t_state			state;
+	char			*data;
+	size_t			cap;
+	size_t			len;
 }	t_node;
 
 #endif /* GET_NEXT_LINE_H */
